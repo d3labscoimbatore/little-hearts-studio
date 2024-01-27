5a424fbda8c21f9db0302868cd4ccc9c58eea495
d3labscoimbatore <d3labscoimbatore@gmail.com>
2024-01-27 13:39:00 +0530

    favicon updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,18 @@
-import { Inter, Josefin_Sans } from "next/font/google";
+import { Josefin_Sans } from "next/font/google";
 import "./globals.css";
 
 const JosefinSans = Josefin_Sans({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Little Hearts Studio",
-  description: "Baby Photography",
+  title:
+    "Expert Baby Photography Services | Newborn to Toddler Shoots | Tamil Nadu | Little Hearts Studio",
+  description:
+    "Capturing precious moments with professional baby photography services. Cherish the joy of parenthood with our expert photographers specializing in newborn, infant, and toddler photo sessions. Book your session today for timeless memories.",
+  icons: {
+    icon: ["/favicon.ico?v=4"],
+    apple: ["/apple-touch-icon.png?v=4"],
+    shortcut: ["apple-touch-icon.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
